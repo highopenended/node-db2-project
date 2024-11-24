52dabb79ef2fd284dbd0a894a92117cb484c2d12
Matt Glenn <highopenended@gmail.com>
2024-11-24 14:48:27 -0500

    Added auto-restarting debugging

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 const express = require("express")
-
 const server = express()
+server.use(express.json())
 
-// DO YOUR MAGIC
+
+
+server.use('*', (req,res,next)=>{
+    next({status:404,message:"not found"})
+    // res.status(404).json({
+    //     message:"not found"
+    // })
+})
+
+server.use((err,req,res,next)=>{ // eslint-disable-line
+    res.status(err.status || 500).json({
+        message:err.message
+    })
+})
+
 
 module.exports = server
