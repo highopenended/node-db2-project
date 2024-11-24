2d649134cfb1ed306cce5be018bba0f7e0fc7e95
Matt Glenn <highopenended@gmail.com>
2024-11-24 14:10:09 -0500

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,13 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl=>{
+    tbl.increments('id') //or just tbl.increments() because 'id' is the default
+    tbl.string('vin', 17).notNullable().unique()
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.numeric('mileage').unsigned().notNullable() // unsigned() makes it so it can't be a negative
+    tbl.string('title', 128)  //.defaultTo('default val')
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
