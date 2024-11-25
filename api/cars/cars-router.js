f5f98998fe918c85fe1543302145c9c9188e4497
Matt Glenn <highopenended@gmail.com>
2024-11-25 00:59:24 -0500

    Started setting up cars-router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,9 +1,15 @@
 
 const express = require('express')
+const Car=require('./cars-model')
 const router = express.Router()
 
-
 router.get('/', async (req, res, next)=>{
+    try{
+        const cars = await Car.getAll()
+        res.status(200).json(cars)    
+    }catch(err){
+        next(err)
+    }
     res.json('getting all cars')
 })
 
